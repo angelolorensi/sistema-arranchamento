9d43057c7ccbc390735d81edae8493cdcb4d74be
angelo lorensi <angelomozza@gmail.com>
2022-10-29 08:53:03 -0300

    alterações no arranchamento individual

--- frontend angular/arranchamento/src/app/components/arranchamento-individual/arranchamento-individual.component.ts
+++ frontend angular/arranchamento/src/app/components/arranchamento-individual/arranchamento-individual.component.ts
@@ -1,6 +1,8 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Militar } from './../../model/Militar';
 import { ArranchamentoService } from './../../services/arranchamento.service';
-import { Component,OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-arranchamento-individual',
@@ -8,22 +10,86 @@ import { Component,OnInit, Output } from '@angular/core';
   styleUrls: ['./arranchamento-individual.component.css'],
 })
 export class ArranchamentoIndividualComponent implements OnInit {
+  form1: FormGroup;
+  form2: FormGroup;
+  form3: FormGroup;
+  form4: FormGroup;
+  form5: FormGroup;
+  form6: FormGroup;
+  form7: FormGroup;
 
-  constructor(private service:ArranchamentoService) {
+  militar?: Militar;
 
+  constructor(
+    private service: ArranchamentoService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {
+    this.form1 = this.formBuilder.group({
+      cafe: [false],
+      almoco: [false],
+      janta: [false],
+      dataRefeicao: [this.data1],
+      militar: [this.militar?.id]
+    });
+    this.form2 = this.formBuilder.group({
+      cafe: [false],
+      almoco: [false],
+      janta: [false],
+      dataRefeicao: [this.data2],
+    });
+    this.form3 = this.formBuilder.group({
+      cafe: [false],
+      almoco: [false],
+      janta: [false],
+      dataRefeicao: [this.data3],
+    });
+    this.form4 = this.formBuilder.group({
+      cafe: [false],
+      almoco: [false],
+      janta: [false],
+      dataRefeicao: [this.data4],
+    });
+    this.form5 = this.formBuilder.group({
+      cafe: [false],
+      almoco: [false],
+      janta: [false],
+      dataRefeicao: [this.data5],
+    });
+    this.form6 = this.formBuilder.group({
+      cafe: [false],
+      almoco: [false],
+      janta: [false],
+      dataRefeicao: [this.data6],
+    });
+    this.form7 = this.formBuilder.group({
+      cafe: [false],
+      almoco: [false],
+      janta: [false],
+      dataRefeicao: [this.data7],
+    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.service.buscarMilitarPorId(14).subscribe({
+      next: (response) => {
+        this.militar = response;
+        console.log(response);
+      },
+    });
+  }
+
+  onSubmit() {
+    this.service.salvarRefeicao(this.form1.value).subscribe(
+      (result) => this.onSuccess(),
+      (error) => this.onError
+    );
+    console.log(this.militar?.id);
+  }
 
   addDays(diasAcrescentados: number, data = new Date()) {
     data.setDate(data.getDate() + diasAcrescentados);
     return data;
-  }
-
-  nome = 'ANGELO';
-
-  getNome(id:number){
-    this.service.buscarMilitarPorId(id).subscribe();
   }
 
   data1 = this.addDays(0, new Date());
@@ -33,4 +99,12 @@ export class ArranchamentoIndividualComponent implements OnInit {
   data5 = this.addDays(4, new Date());
   data6 = this.addDays(5, new Date());
   data7 = this.addDays(6, new Date());
+
+  onSuccess() {
+    this.snackBar.open('Arranchamento salvo com sucesso', '', { duration: 5000 });
+  }
+
+  onError() {
+    this.snackBar.open('Erro ao se arranchar', '', { duration: 5000 });
+  }
 }
